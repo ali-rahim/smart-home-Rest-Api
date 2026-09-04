251aa2d65df14a0530f81a2522d7508512174a37
Ali Rahim <ar5478246@gmail.com>
2026-09-04 17:46:08 +0330

    Revert "refactor تمام شد"
    This reverts commit 53b308211e65f87898e8be8ba0f612568de1d6f6.

--- SmartHoe_dbcontex/RequestDtos.cs
+++ SmartHoe_dbcontex/RequestDtos.cs
@@ -1,6 +1,6 @@
 ﻿namespace smart_home_Asp.net.Dtos
 {
-    public record CreateHomeRequest(string Name);
+    public record CreateHomeRequest(string Name, int? Id);
     public record RoomRequest(string Name);
     public record UpdateHomeRequest(string Name);
     public record CreateDeviceRequest(string Name, string DeviceType, string ExternalId);
