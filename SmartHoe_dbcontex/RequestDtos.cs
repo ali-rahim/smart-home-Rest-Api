26f4f9eb5ee30676301590972ad51c13e141dada
Ali Rahim <ar5478246@gmail.com>
2026-09-04 15:54:59 +0330

    rifactor4

--- SmartHoe_dbcontex/RequestDtos.cs
+++ SmartHoe_dbcontex/RequestDtos.cs
@@ -1,11 +1,7 @@
 ﻿namespace smart_home_Asp.net.Dtos
 {
     public record RoomRequest(string Name);
-
-
     public record UpdateHomeRequest(string Name);
-
-
     public record CreateDeviceRequest(string Name, string DeviceType, string ExternalId);
     public record UpdateDeviceRequest(string Name, string ExternalId);
 }
