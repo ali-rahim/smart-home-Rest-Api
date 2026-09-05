5a8a3c8ae6d17ab5bc5fed9634fcc92b21990da7
ali-rahim <148591358+ali-rahim@users.noreply.github.com>
2026-09-05 18:03:13 +0330

    Inject device communicator into command manager

--- services/DeviceCommandManager.cs
+++ services/DeviceCommandManager.cs
@@ -1,17 +1,15 @@
-﻿using Entity_class.Domain.Devices.Base;
+using DeviceCommunicator;
+using Entity_class.Domain.Devices.Base;
 using Microsoft.EntityFrameworkCore;
 using smart_home_Asp.net.Domain.Devices.ability_interfaces;
 using smart_home_Asp.net.Domain.Devices.Base;
 using SmartHoe_dbcontex;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace services
 {
-    public class DeviceCommandManager(SmartHome_dbcontex sdx)
+    public class DeviceCommandManager(
+        SmartHome_dbcontex sdx,
+        IDeviceCommunicator communicator)
     {
         public Task<Device?> TurnOnAsync(int deviceId) => SetSwitchAsync(deviceId, turnOn: true);
 
@@ -24,6 +22,17 @@ namespace services
 
             if (device is not Iswitchable switchable)
                 throw new InvalidOperationException($"Device {deviceId} does not support switching.");
+
+            if (string.IsNullOrWhiteSpace(device.ExternalId))
+                throw new InvalidOperationException($"Device {deviceId} does not have an external ID.");
+
+            var result = turnOn
+                ? await communicator.TurnOnAsync(device.ExternalId)
+                : await communicator.TurnOffAsync(device.ExternalId);
+
+            if (!result.Success)
+                throw new InvalidOperationException(
+                    result.Error ?? $"Device {deviceId} failed to execute the command.");
 
             if (turnOn) switchable.Turn_on();
             else switchable.Turn_off();
